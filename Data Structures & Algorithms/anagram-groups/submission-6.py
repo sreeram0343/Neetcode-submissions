455e9843e766cea38462112bb75ad2e128d2519c
Sreeram M R <sreerammurali2005@gmail.com>
2026-07-04 10:29:29 +0530

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.py
@@ -0,0 +1,20 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        group = defaultdict(list)
+
+        for word in strs:
+
+            count = [0] * 26
+
+            for char in word:
+
+                charin = ord(char) - ord('a')
+
+                count[charin] += 1
+
+
+            group[tuple(count)].append(word)
+
+
+        return list(group.values())     
+        
